0eacb86e1c5180c2fe54f55578b51ab56a56056d
=Prativa Neupane <prativaneupane4@gmail.com>
2025-01-18 21:11:04 +0545

    Update index.html, main.js, and style.css

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
-function callback() {
-  const appElement = document.querySelector("#app");
-  const ecommerceHtml = "<h2>ecommerce app</h2>";
-  appElement.innerHTML = ecommerceHtml;
+
+let navlinks = document.getElementsByClassName("nav_links")[0];  // Get the first element with class 'nav_links'
+
+navlinks.style.maxHeight = "0px";  // Set initial height to 0
+
+function togglemenu() {
+  if (navlinks.style.maxHeight == "0px") {
+    navlinks.style.maxHeight = "300px";  // Expand menu
+  } else {
+    navlinks.style.maxHeight = "0px";  // Collapse menu
+  }
 }
-document.addEventListener("DOMContentLoaded", callback);
