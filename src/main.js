9aecb4b034022054c8170a38742bdbb52e73b4a2
=Prativa Neupane <prativaneupane4@gmail.com>
2025-01-27 00:28:28 +0545

    login and signup page has been added

--- src/main.js
+++ src/main.js
@@ -3,11 +3,26 @@ document.addEventListener("DOMContentLoaded", function () {
   const crossBtn = document.getElementById("cross_btn");
   const mbNavbar = document.getElementById("mb_navbar");
 
-  hamburgerBtn.addEventListener("click", function (e) {
+  hamburgerBtn.addEventListener("click", function () {
     mbNavbar.style.display = "block";
   });
 
-  crossBtn.addEventListener("click", function (e) {
+  crossBtn.addEventListener("click", function () {
     mbNavbar.style.display = "none";
+  });
+
+  window.addEventListener("resize", () => {
+    if (mbNavbar.clientWidth > 1024) {
+      mbNavbar.style.display = "none";
+    }
+  });
+
+  // Functionality for the sign-in button
+  const loginbtn = document.getElementById("button-container");
+  const flipcard = document.getElementById("flipCardContainer");
+
+  loginbtn.addEventListener("click", function () {
+    // Toggle the visibility of the flipcard
+    flipcard.classList.toggle("show");
   });
 });
